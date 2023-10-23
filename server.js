d1d52aeb207984a000fbe9b19bf2d18167253e8d
gh.park <gh.park@quintet.co.kr>
2023-10-23 15:52:41 +0900

    Feat : 이미지 저장을 위한 MongoDB 연결

--- server.js
+++ server.js
@@ -1,13 +1,43 @@
-
+require("dotenv").config();
 
 const express = require("express");
+const multer = require("multer");
+const {v4 : uuid} = require("uuid");
+const mime = require("mime-types");
+const mongoose = require("mongoose");
+const Image = require("./models/image");
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => cb(null, "./uploads"),
+    filename: (req, file, cb) => 
+        cb(null, `${uuid()}.${mime.extension(file.mimetype)}`),
+});
+
+const upload = multer({storage});
 
 const app = express();
 const PORT = 5000;
 
-app.post("/upload", (req, res) => {
-    console.log("/upload called!");
-    res.json({result :  "Success"})
-})
+console.log(process.env)
 
-app.listen(PORT, () => console.log(" Express server listening on PORT " + PORT))
+mongoose.connect(
+        process.env.MONGO_URI,
+    )
+    .then(()=>{
+        console.log("MongoDB Connected")
+        
+        app.post("/upload", upload.single("image"), async (req, res) => {
+            console.log("/upload called!");
+            await new Image({
+                key:req.file.filename, 
+                originalFileName:req.file.originalname
+            }).save();
+            console.log(req.file)
+            res.json(req.file);
+        });
+        
+        app.listen(PORT, () => console.log("Express server listening on PORT " + PORT));
+    
+    })
+    .catch((err) => console.log(err));
+
