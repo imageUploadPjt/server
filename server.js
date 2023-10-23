ef7ba10ca50ddc83c651e3ab313cdc9cdf4a58f3
gh.park <gh.park@quintet.co.kr>
2023-10-23 14:14:37 +0900

    Feat : 이미지 업로드 API 설계 - 백엔드 서버에 저장

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 
-
 const express = require("express");
+const multer = require("multer");
+const upload = multer({dest:"uploads"});
 
 const app = express();
 const PORT = 5000;
 
-app.post("/upload", (req, res) => {
+app.post("/upload", upload.single("image"), (req, res) => {
     console.log("/upload called!");
-    res.json({result :  "Success"})
+    console.log(req.file);    
+    res.json({result :  "Success", "req.file" : req.file});
 })
 
 app.listen(PORT, () => console.log("Express server listening on PORT " + PORT))
